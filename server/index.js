e6b3333fee171757152ef212573b799d8ed4152c
Manasvi0910 <97155655+Manasvi0910@users.noreply.github.com>
2023-05-19 18:01:10 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -34,5 +34,5 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    User.insertMany(dataUser);
+    
 }).catch((error) => console.log(`${error} did not connect`))
